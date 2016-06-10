f7a3b00b828b2330d163d1ab8019c955c49eacfc
blaked <dorian.becker@gadz.org>
2016-06-10 15:27:21 +0200

    add group validations

--- app/controllers/api/v2/groups_controller.rb
+++ app/controllers/api/v2/groups_controller.rb
@@ -78,6 +78,6 @@ class Api::V2::GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:guid, :name, :description)
+      params.require(:group).permit(:guid, :name, :short_name, :description)
     end
 end
--- app/models/master_data/group.rb
+++ app/models/master_data/group.rb
@@ -1,3 +1,25 @@
 class MasterData::Group < MasterData::Base
-	has_and_belongs_to_many :accounts
+  #relations
+  has_and_belongs_to_many :accounts
+
+  #callbacks
+  before_validation :generate_guid_if_empty
+  
+  #model validations
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :guid, uniqueness: true
+  validates :short_name, uniqueness: true, presence: true
+
+
+  def generate_guid_if_empty
+    self.guid ||= self.generate_guid
+  end
+
+  def generate_guid
+    self.guid = loop do
+      random_guid = SecureRandom.uuid
+      break random_guid unless MasterData::Group.exists?(guid: random_guid)
+    end
+  end
 end
