8cfb86619d90db152a4593dbfc1dd434596abc2e
blaked <dorian.becker@gadz.org>
2016-06-10 14:50:25 +0200

    add role validations

--- app/models/master_data/role.rb
+++ app/models/master_data/role.rb
@@ -1,4 +1,8 @@
 class MasterData::Role < MasterData::Base
+	#relations
 	has_and_belongs_to_many :accounts
 
+	#model validations
+	validates :name, :application, :description, presence: true
+	validates :name, uniqueness: true
 end
