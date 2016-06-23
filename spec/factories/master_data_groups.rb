ffed05448589ff04c49787c7eabb083534d114ff
blaked <dorian.becker@gadz.org>
2016-06-23 13:02:33 +0200

    group factory

--- spec/factories/master_data_groups.rb
+++ spec/factories/master_data_groups.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :master_data_group, class: 'MasterData::Group' do
     guid "MyString"
-    name "MyString"
+    name Faker::Team.creature
+    short_name Faker::Team.creature.downcase
     description "MyString"
   end
 end
