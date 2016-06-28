f6ea0dc6c7b4d7b570f3a01b04c340b248552fbf
blaked <dorian.becker@gadz.org>
2016-06-28 16:10:58 +0200

    add authorization check

--- app/controllers/api/v2/base_controller.rb
+++ app/controllers/api/v2/base_controller.rb
@@ -7,9 +7,16 @@ class Api::V2::BaseController < ApplicationController
   before_action :authenticate_client!
   before_filter :require_login
 
+  # check if authorize is called for every action
+  after_action :verify_authorized
+
+  #To send context to serializer. Used to manage fields' filter with special ACL
+  serialization_scope :view_context
+
   private
 
   def require_login
+    # TODO : remove lines bellow
     puts "==============================================="
     puts current_client.name
     unless current_client
