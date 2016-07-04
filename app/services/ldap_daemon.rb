0fbe22f593afd54d1c659d1a50d3d58304f1008a
blaked <dorian.becker@gadz.org>
2016-07-04 15:50:44 +0200

    update rabbitmq message structure

--- app/services/ldap_daemon.rb
+++ app/services/ldap_daemon.rb
@@ -7,17 +7,17 @@ class LdapDaemon
   end
 
   def request_account_update(account)
-    msg={id_soce: account.id_soce.to_s}
+    msg={account: {id_soce: account.id_soce.to_s}}
     @message_sender.send_message(msg, 'request.ldapd.update')
   end
 
   def request_group_update(group)
-    msg={short_name: group.short_name.to_s}
+    msg={group: {short_name: group.short_name.to_s}}
     @message_sender.send_message(msg, 'request.ldapd.update')
   end
 
   def request_role_update(role)
-    msg={name: role.name.to_s, application: role.application.to_s}
+    msg={role: {name: role.name.to_s, application: role.application.to_s}}
     @message_sender.send_message(msg, 'request.ldapd.update')
   end
 
