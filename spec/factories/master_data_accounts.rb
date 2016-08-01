e9f4cc254fc9708ddc344beaf03b047c4e063741
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-08-01 16:52:54 +0200

    Update MasterData::Account Factory

--- spec/factories/master_data_accounts.rb
+++ spec/factories/master_data_accounts.rb
@@ -4,13 +4,13 @@ require 'digest/sha1'
 FactoryGirl.define do
   factory :master_data_account, class: 'MasterData::Account' do
       uuid {SecureRandom.uuid}
-      hruid {"#{Faker::Name.first_name}.#{Faker::Name.last_name}.#{(1900..2000).to_a.sample}"}
-      firstname Faker::Name.first_name
-      lastname Faker::Name.last_name
+      hruid {"#{firstname}.#{lastname}.#{(1900..2000).to_a.sample}"}
+      firstname {Faker::Name.first_name}
+      lastname {Faker::Name.last_name}
       is_gadz true
       # id_soce {(2000..20000).to_a.sample}
       enabled true
       password {Digest::SHA1.hexdigest uuid}
-      email {Faker::Internet.safe_email("#{Faker::Name.first_name} #{Faker::Name.last_name} "+rand(1..99).to_s)}
+      email {Faker::Internet.safe_email("#{firstname} #{lastname} "+rand(1..99).to_s)}
   end
 end
