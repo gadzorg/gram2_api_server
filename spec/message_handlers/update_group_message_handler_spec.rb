7a54f9d1a1f5f96958b77833bd42833e149ad5f4
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-08-24 14:34:35 +0200

    fix spec typo

--- spec/message_handlers/update_group_message_handler_spec.rb
+++ spec/message_handlers/update_group_message_handler_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe UpdateGroupMessageHandler, type: :message_handler do
 
   let(:message){GorgService::Message.new(data:valid_message_payload)}
 
-  context "whren there is an invalid payload" do
+  context "when there is an invalid payload" do
     let(:message){GorgService::Message.new(data:invalid_message_payload)}
 
     it "raise an hardfail error" do
@@ -38,53 +38,57 @@ RSpec.describe UpdateGroupMessageHandler, type: :message_handler do
       let(:user2) {FactoryGirl.create(:master_data_account)}
       let(:user3) {FactoryGirl.create(:master_data_account)}
 
+      let(:valid_message_payload) {
+         {group_uuid:group.uuid,name:"Dragon's Team",short_name:"dragon",description:"Equipe",members:members_uuid_list}
+      }
       context "without members" do
         let(:members_uuid_list) {[]}
 
-        let(:valid_message_payload) {
-           {group_uuid:group.uuid,name:"Dragon's Team",short_name:"dragon",description:"Equipe",members:members_uuid_list}
-        }
 
-        describe "update group data" do
-          before(:each) do
-            subject
-            group.reload
-          end
 
-          it "update name" do
-            expect(group.name).to eq("Dragon's Team")
-          end
-
-          it "update short_name" do
-            expect(group.short_name).to eq("dragon")
-          end
-
-          it "update description" do
-            expect(group.description).to eq("Equipe")
-          end
+        before(:each) do
+          subject
+          group.reload
         end
 
-        context "with members" do
-          let(:members_uuid_list) {[user1.uuid,user3.uuid]}
-          before(:each) {group.accounts << [user1,user2]}
-
-          before(:each) do
-            subject
-            group.reload
-          end
-
-          it "add new members" do
-            expect(group.accounts).to include(user3)
-          end
-
-          it "delete removed members" do
-            expect(group.accounts).not_to include(user2)
-          end
-
+        it "update name" do
+          expect(group.name).to eq("Dragon's Team")
         end
 
+        it "update short_name" do
+          expect(group.short_name).to eq("dragon")
+        end
+
+        it "update description" do
+          expect(group.description).to eq("Equipe")
+        end
+      end
+
+
+      context "with members" do
+        let(:members_uuid_list) {[user1.uuid,user3.uuid]}
+        before(:each) {group.accounts << [user1,user2]}
+
+        before(:each) do
+          subject
+          group.reload
+        end
+
+        it "keep existing members" do
+          expect(group.accounts).to include(user1)
+        end
+
+        it "add new members" do
+          expect(group.accounts).to include(user3)
+        end
+
+        it "delete removed members" do
+          expect(group.accounts).not_to include(user2)
+        end
 
       end
+
+
     end
   end
 
