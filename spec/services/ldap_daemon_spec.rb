432c8ece32c29b8c21d4050411a8e5e321219350
Alexandre Narbonne <alexandre.narbonne@yahoo.fr>
2016-08-30 10:43:11 +0200

    Update ldap_daemon_spec.rb

--- spec/services/ldap_daemon_spec.rb
+++ spec/services/ldap_daemon_spec.rb
@@ -8,20 +8,13 @@ RSpec.describe LdapDaemon, type: :service do
     acc=FactoryGirl.build(:master_data_account, id_soce: 1234)
     ld=LdapDaemon.new(message_sender: message_sender)
     ld.request_account_update(acc)
-    expect(message_sender).to have_received.send_message({account: {uuid: acc.uuid.to_s}}, 'request.ldapd.update')
+    expect(message_sender).to have_received.send_message({uuid: acc.uuid.to_s}, 'request.ldapd.account.update')
   end
 
   it "request a message sending to the message sender for group update" do
     group=FactoryGirl.build(:master_data_group, short_name: "test_group")
     ld=LdapDaemon.new(message_sender: message_sender)
     ld.request_group_update(group)
-    expect(message_sender).to have_received.send_message({group: {uuid: group.uuid.to_s}}, 'request.ldapd.update')
-  end
-
-  it "request a message sending to the message sender for role update" do
-    role=FactoryGirl.build(:master_data_role, name: "test_role", application: "test_app")
-    ld=LdapDaemon.new(message_sender: message_sender)
-    ld.request_role_update(role)
-    expect(message_sender).to have_received.send_message({role: {uuid: role.uuid.to_s}}, 'request.ldapd.update')
+    expect(message_sender).to have_received.send_message({uuid: group.uuid.to_s}, 'request.ldapd.group.update')
   end
 end
