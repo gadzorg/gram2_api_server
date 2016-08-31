5077362c01dc948200f9fd0dec8ac384ea7045d2
blaked <dorian.becker@gadz.org>
2016-08-31 13:49:49 +0200

    add like search

--- app/controllers/api/v2/accounts_controller.rb
+++ app/controllers/api/v2/accounts_controller.rb
@@ -7,11 +7,16 @@ class Api::V2::AccountsController < Api::V2::BaseController
   # GET /api/v2/accounts
   # GET /api/v2/accounts.json
   def index
-    search = params.permit(:uuid, :hruid, :id_soce, :enabled, :lastname, :firstname, :birthname, :birth_firstname, :email, :gapps_id, :password, :birthdate, :deathdate, :gender, :is_gadz, :is_student, :school_id, :is_alumni, :date_entree_ecole, :date_sortie_ecole, :ecole_entree, :buque_texte, :buque_zaloeil, :gadz_fams, :gadz_fams_zaloeil, :gadz_proms_principale, :gadz_proms_secondaire, :description, :alias)
+    filter = params.permit(:hruid, :id_soce, :enabled, :lastname, :firstname, :birthname, :birth_firstname, :email, :gapps_id, :password, :birthdate, :deathdate, :gender, :is_gadz, :is_student, :school_id, :is_alumni, :date_entree_ecole, :date_sortie_ecole, :ecole_entree, :buque_texte, :buque_zaloeil, :gadz_fams, :gadz_fams_zaloeil, :gadz_proms_principale, :gadz_proms_secondaire, :description, :alias)
     if @group
       @accounts = @group.accounts
     else
-      @accounts = MasterData::Account.all.where(search).includes(:groups, :alias, :roles)
+
+      if params[:exact_search] == "false"
+        @accounts = MasterData::Account.like(filter).includes(:groups, :alias, :roles)
+      else
+        @accounts = MasterData::Account.where(filter).includes(:groups, :alias, :roles)
+      end
     end
     authorize @accounts, :index?
     respond_to do |format|
