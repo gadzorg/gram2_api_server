2fe0c11c66ce88028237151cee3d4575b2313aec
Guillaume Dufloux <guillaume.dufloux@wopata.com>
2018-02-16 16:14:46 +0100

    Fix strong parameters indentation in accounts_controller

--- app/controllers/api/v2/accounts_controller.rb
+++ app/controllers/api/v2/accounts_controller.rb
@@ -198,7 +198,38 @@ class Api::V2::AccountsController < Api::V2::BaseController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def api_v2_account_params
-      params.require(:account).permit(:uuid, :hruid, :id_soce, :enabled, :password, :lastname, :firstname, :birthname, :birth_firstname, :email, :gapps_id, :password, :birthdate, :deathdate, :gender, :is_gadz, :is_student, :school_id, :is_alumni, :date_entree_ecole, :date_sortie_ecole, :ecole_entree, :buque_texte, :buque_zaloeil, :gadz_fams, :gadz_fams_zaloeil, :gadz_proms_principale, :gadz_proms_secondaire, :avatar_url, :description)
+      params.require(:account).permit(
+        :uuid,
+        :hruid,
+        :id_soce,
+        :enabled,
+        :password,
+        :lastname,
+        :firstname,
+        :birthname,
+        :birth_firstname,
+        :email,
+        :gapps_id,
+        :password,
+        :birthdate,
+        :deathdate,
+        :gender,
+        :is_gadz,
+        :is_student,
+        :school_id,
+        :is_alumni,
+        :date_entree_ecole,
+        :date_sortie_ecole,
+        :ecole_entree,
+        :buque_texte,
+        :buque_zaloeil,
+        :gadz_fams,
+        :gadz_fams_zaloeil,
+        :gadz_proms_principale,
+        :gadz_proms_secondaire,
+        :avatar_url,
+        :description
+      )
     end
 
     def  show_password_hash?
