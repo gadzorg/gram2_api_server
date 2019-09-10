0f4f2090cf96b8301a3bf5552a5c775dd486b9ab
Colin Darie <colin@darie.eu>
2019-09-10 19:34:34 +0200

    Test: increase randomness in Group short_name factory

--- spec/factories/master_data_groups.rb
+++ spec/factories/master_data_groups.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :master_data_group, class: "MasterData::Group" do
     uuid { SecureRandom.uuid }
     name { Faker::Team.creature + Faker::Name.first_name }
-    short_name { Faker::Team.creature.downcase }
+    short_name { (Faker::Team.creature + Faker::Name.first_name).downcase  }
     description { Faker::Company.catch_phrase }
   end
 end
