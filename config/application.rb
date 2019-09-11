5f29e41ba568520d3006ebcbe276bf8526221188
Colin Darie <colin@darie.eu>
2019-09-11 16:23:33 +0200

    Update Rails 5.2 configuration files.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,9 +12,10 @@ module GorgGramApiServer2
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+    
     config.autoload_paths << Rails.root.join("lib/gorg_rabbitmq_notifier")
   end
 end
