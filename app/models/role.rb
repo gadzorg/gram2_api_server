497143111f22be588962df6d1625b8768f3a4c23
Colin Darie <colin@darie.eu>
2019-09-11 15:42:06 +0200

    Role belongs_to :resource is optional (override new Rails 5 default)

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,7 @@
 class Role < ApplicationRecord
   has_and_belongs_to_many :clients, join_table: :clients_roles
 
-  belongs_to :resource, polymorphic: true
+  belongs_to :resource, polymorphic: true, optional: true
 
   validates :resource_type,
             inclusion: { in: Rolify.resource_types }, allow_nil: true
